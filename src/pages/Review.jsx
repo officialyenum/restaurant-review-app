342246b23a8af62ad50198a36e7fdc3c401548f6
vickzuzo <vickzuzo@gmail.com>
2023-03-10 10:47:18 +0100

    fix reviews page

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,26 +1,23 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ReviewsList } from "../components/ReviewComponents/ReviewsList";
 import { getLatestTenReviews } from "../store/actions/record.action";
-import classes from "./Home.module.css";
+import classes from "./Review.module.css";
 
 const Review = () => {
   const reviews = useSelector((state) => state.record.selectedRecordReviews);
   const dispatch = useDispatch();
   useEffect(() => {
-    console.log('working');
+    console.log("working");
     dispatch(getLatestTenReviews());
-  }, [dispatch])
+  }, [dispatch]);
   return (
-    <>
+    <div className={classes.reviewContainer}>
       <div className={classes.reviewWrapper}>
-        <h2 className={classes.review}>
-          Latest Reviews.
-        </h2>
+        <h2 className={classes.review}>Latest Reviews.</h2>
       </div>
-      {reviews && (<ReviewsList reviews={reviews} showRestaurant={true} />)}
-      
-    </>
+      {reviews && <ReviewsList reviews={reviews} showRestaurant={true} />}
+    </div>
   );
 };
 
