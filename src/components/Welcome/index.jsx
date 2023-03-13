2754944b396a1f3d27c9b72ca51d677f63140241
officialyenum <oponechukwuyenum@gmail.com>
2023-03-13 07:52:27 +0100

    Fix Hygiene Text

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -12,7 +12,7 @@ export const Welcome = () => {
         <div className={classes.rightContainer}>
           <h1>
             <span>Welcome</span>
-            <br /> to Restaurant Hygiene Review in Bristol
+            <br /> to Food Hygiene Review in Bristol
           </h1>
           <p>To enhance food hygiene and safety across restaurants in Bristol City by ensuring diners are given access for FSA scheme ratings about restaurants in Bristol City. While allowing them to drop reviews/ratings that are visible to be shared on social media and back to the FSA for updates.s</p>
         </div>
