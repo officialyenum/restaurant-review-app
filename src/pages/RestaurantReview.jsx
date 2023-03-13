f7d9149fd388faf9f816eb10b650a70b1a314555
officialyenum <oponechukwuyenum@gmail.com>
2023-03-13 08:28:28 +0100

    Revert Changes

--- src/pages/RestaurantReview.jsx
+++ src/pages/RestaurantReview.jsx
@@ -69,32 +69,26 @@ const RestaurantReview = () => {
             <div className={classes.hygiene_card}>
               <i class="fa-solid fa-star"></i>
               <h1>Rating</h1>
-              {record.rating}
-              {record.rating >= 0 && (
-                <div className={classes.starWrapper}>
-                  <StarRating rating={record?.rating} />
-                </div>
-              )}
+              {record?.rating}
+              <div className={classes.starWrapper}>
+                <StarRating rating={record?.rating} />
+              </div>
             </div>
             <div className={classes.hygiene_card}>
               <i class="fa-solid fa-hands-holding-circle"></i>
               <h1>Hygiene Score</h1>
-              {record.scoresHygiene}
-              {record.scoresHygiene >= 0 && (
-                <div className={classes.starWrapper}>
-                  <StarRating rating={record?.scoresHygiene} />
-                </div>
-              )}
+              {record?.scoresHygiene}
+              <div className={classes.starWrapper}>
+                <StarRating rating={record?.scoresHygiene} />
+              </div>
             </div>
             <div className={classes.hygiene_card}>
               <i className="fa fa-search"></i>
               <h1>Structural Score</h1>
-              {record.scoresStructural}
-              {record.scoresStructural >= 0 && (
-                <div className={classes.starWrapper}>
-                  <StarRating rating={record?.scoresStructural} />
-                </div>
-              )}
+              {record?.scoresStructural}
+              <div className={classes.starWrapper}>
+                <StarRating rating={record?.scoresStructural} />
+              </div>
             </div>
           </div>
           <div className={classes.threads}>{reviews.length} Reviews</div>
